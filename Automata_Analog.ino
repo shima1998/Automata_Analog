d0a8a655c90a3150f8f095155820b4fc540c442b
email <b1017216@gmail.com>
2018-10-09 20:58:17 +0900

    LED点灯を試行　だめかもしれない

--- Automata_Analog.ino
+++ Automata_Analog.ino
@@ -9,16 +9,19 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);//サイト参照
   //pinMode(light,INPUT);//サイト参照
-  // pinMode(led, OUTPUT);//LED点灯用
+  pinMode(led, OUTPUT);//LED点灯用
   }
 
 void loop() {
   // put your main code here, to run repeatedly:
   //int val = digitalRead(light);//サイト参照
-  int val = analogRead(7)//アナログ用
+  int val = analogRead(7);//アナログ用
   Serial.println(val);//サイト参照
 
-  ;
+  if(val >= 500){
+    digitalWrite(led, HIGH);
+  }
+  
 
 /*
   if(val == 1){//光を浴びない→LED消灯 光を浴びる→LED点灯
